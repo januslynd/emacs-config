669d7a8987d3e9c04e47ed8e7c9fc819145b33a7
Mario Garcia <mario.ggar@gmail.com>
2018-06-29 16:51:01 +0200

    Add flycheck with eslint checker

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
-;; Personal emacs configuration
+;;; package --- Something
+;;; Commentary:
 
 ;; GLOBAL SETTINGS
+;;; Code:
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -25,7 +27,6 @@
 (setq-default sgml-basic-offset 4)
 (setq-default nxml-child-indent tab-width)
 (setq-default nxml-outline-child-indent tab-width)
-(setq js-indent-level 4)
 (setq x-stretch-cursor 1)
 
 ;; Zoom in/out
@@ -36,10 +37,10 @@
 
 (setq package-archives
       '(
-	("gnu" . "http://elpa.gnu.org/packages/")
-	("org" . "http://orgmode.org/elpa/")
+;;	("gnu" . "http://elpa.gnu.org/packages/")
+;;	("org" . "http://orgmode.org/elpa/")
 	("melpa-stable" . "http://stable.melpa.org/packages/")
-	("melpa" . "http://melpa.org/packages/")
+;;	("melpa" . "http://melpa.org/packages/")
 	)
       )
 (package-initialize)
@@ -50,7 +51,7 @@
 
 (require 'use-package)
 
-(load-theme 'misterioso)
+(load-theme 'whiteboard)
 
 ;; GENERAL PURPOSE PACKAGES
 
@@ -78,12 +79,29 @@
   :bind (([f8] . neotree-toggle))
   :config (setq neo-theme 'ascii))
 
-;; PROGRAMMING LANGUAGES
+;; DOCKER
+
+(use-package dockerfile-mode
+  :ensure t)
+
+;; EDITING
 
 (use-package editorconfig
   :ensure t
   :config
   (editorconfig-mode 1))
+
+(use-package yaml-mode
+  :ensure t
+  :mode "\\.yml\\'")
+
+(put 'narrow-to-region 'disabled nil)
+
+(use-package markdown-mode
+  :ensure t
+  :mode ("\\.md\\'" "\\.MD\\'"))
+
+;; PROGRAMMING LANGUAGES
 
 (use-package groovy-mode
   :ensure t
@@ -101,25 +119,71 @@
   :ensure t
   :mode ("\\.clj\\'" "\\.cljs\\'"))
 
-;; DOCKER
+;; FRONT DEVELOPMENT
 
-(use-package dockerfile-mode
+;; eslint will not work if using emacs in xorg mode
+;; because in that mode shell variables are not
+;; available, therefore emacs won't be able to find
+;; eslint executable
+(use-package exec-path-from-shell
+  :ensure t
+  :init
+  (when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize)))
+
+(use-package flycheck
+  :ensure t
+  :init
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; web-mode should have the javascript-eslint checker activated
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;;
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  "Use local eslint from node_modules before global."
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+;; sometimes latest flycheck can't find the xml parser to parse eslint output
+(setq flycheck-xml-parser 'flycheck-parse-xml-region)
+
+(use-package js2-mode
   :ensure t)
 
-;; EDITING
+(use-package json-mode
+  :ensure t)
 
-(use-package yaml-mode
+(use-package web-mode
   :ensure t
-  :mode "\\.yml\\'")
-(put 'narrow-to-region 'disabled nil)
+  :mode ("\\.js\\'" "\\.jsx\\'"))
 
-(use-package markdown-mode
-  :ensure t
-  :mode ("\\.md\\'" "\\.MD\\'"))
+;; GENERATED CODE
 
-(require 'kotlin-mode)
-;;(use-package kotlin-mode
-;;  :ensure t
-;;  :load-path "elpa/kotlin-mode-0.1.0/"
-;;  :mode ("\\.kt\\'"))
-(add-to-list 'auto-mode-alist '("\\.kt\\'" . kotlin-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck js2-mode json-mode web-mode markdown-mode yaml-mode dockerfile-mode neotree multiple-cursors ag projectile magit use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
